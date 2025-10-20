f61e0161caaeae7a501006ce2d7bd4c475082f06
Jandi3258 <pawel.jandula@vp.pl>
2025-10-20 22:29:43 +0200

    Zadanie 1

--- src/main/java/uj/wmii/pwj/introduction/Banner.java
+++ src/main/java/uj/wmii/pwj/introduction/Banner.java
@@ -1,9 +1,282 @@
 package uj.wmii.pwj.introduction;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Banner {
-
-    public String[] toBanner(String input) {
-        return null;
+    private final Map<Character, String[]> MapaLiter = new HashMap<>();
+    public Banner() {
+        zamiana(); 
     }
+    private void zamiana(){
+        MapaLiter.put('A', new String[]{
+                "   #   ",
+                "  # #  ",
+                " #   # ",
+                "#     #",
+                "#######" ,
+                "#     #",
+                "#     #"
+        });
+        MapaLiter.put('B', new String[]{
+                "###### ",
+                "#     #",
+                "#     #",
+                "###### ",
+                "#     #",
+                "#     #",
+                "###### "
+        });
+        MapaLiter.put('C', new String[]{
+                " ##### ",
+                "#     #",
+                "#      ",
+                "#      ",
+                "#      ",
+                "#     #",
+                " ##### "
+        });
+        MapaLiter.put('D', new String[]{
+                "###### ",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "###### "
+        });
+        MapaLiter.put('E', new String[]{
+                "#######",
+                "#      ",
+                "#      ",
+                "#####  ",
+                "#      ",
+                "#      ",
+                "#######"
+        });
+        MapaLiter.put('F', new String[]{
+                "#######",
+                "#      ",
+                "#      ",
+                "#####  ",
+                "#      ",
+                "#      ",
+                "#      "
+        });
+        MapaLiter.put('G', new String[]{
+                " ##### ",
+                "#     #",
+                "#      ",
+                "#  ####",
+                "#     #",
+                "#     #",
+                " ##### "
+        });
+        MapaLiter.put('H', new String[]{
+                "#     #",
+                "#     #",
+                "#     #",
+                "#######",
+                "#     #",
+                "#     #",
+                "#     #"
+        });
+        MapaLiter.put('I', new String[]{
+                "###",
+                " # ",
+                " # ",
+                " # ",
+                " # ",
+                " # ",
+                "###"
+        });
+        MapaLiter.put('J', new String[]{
+                "      #",
+                "      #",
+                "      #",
+                "      #",
+                "#     #",
+                "#     #",
+                " ##### "
+        });
+        MapaLiter.put('K', new String[]{
+                "#    #",
+                "#   # ",
+                "#  #  ",
+                "###   ",
+                "#  #  ",
+                "#   # ",
+                "#    #"
+        });
+        MapaLiter.put('L', new String[]{
+                "#      ",
+                "#      ",
+                "#      ",
+                "#      ",
+                "#      ",
+                "#      ",
+                "#######"
+        });
+        MapaLiter.put('M', new String[]{
+                "#     #",
+                "##   ##",
+                "# # # #",
+                "#  #  #",
+                "#     #",
+                "#     #",
+                "#     #" 
+        });
+        MapaLiter.put('N', new String[]{
+                "#     #",
+                "##    #",
+                "# #   #",
+                "#  #  #",
+                "#   # #",
+                "#    ##",
+                "#     #"
+        });
+        MapaLiter.put('O', new String[]{
+                "#######",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#######"
+        });
+        MapaLiter.put('P', new String[]{
+                "###### ",
+                "#     #",
+                "#     #",
+                "###### ",
+                "#      ",
+                "#      ",
+                "#      "
+        });
+        MapaLiter.put('Q', new String[]{
+                " ##### ",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#   # #",
+                "#    # ",
+                " #### #"
+        });
+        MapaLiter.put('R', new String[]{
+                "###### ",
+                "#     #",
+                "#     #",
+                "###### ",
+                "#   #  ",
+                "#    # ",
+                "#     #" 
+        });
+        MapaLiter.put('S', new String[]{
+                " ##### ",
+                "#     #",
+                "#      ",
+                " ##### ",
+                "      #",
+                "#     #",
+                " ##### "
+        });
+        MapaLiter.put('T', new String[]{
+                "#######",
+                "   #   ",
+                "   #   ",
+                "   #   ",
+                "   #   ",
+                "   #   ",
+                "   #   "
+        });
+        MapaLiter.put('U', new String[]{
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                " ##### "
+        });
+        MapaLiter.put('V', new String[]{
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                " #   # ",
+                "  # #  ",
+                "   #   "
 
+        });
+        MapaLiter.put('W', new String[]{
+                "#     #",
+                "#  #  #",
+                "#  #  #",
+                "#  #  #",
+                "#  #  #",
+                "#  #  #",
+                " ## ## "
+        });
+        MapaLiter.put('X', new String[]{  
+                "#     #",
+                " #   # ",
+                "  # #  ",
+                "   #   ",
+                "  # #  ",
+                " #   # ",
+                "#     #"
+        });
+        MapaLiter.put('Y', new String[]{
+                "#     #",
+                " #   # ",
+                "  # #  ",
+                "   #   ",
+                "   #   ",
+                "   #   ",
+                "   #   "
+        });
+        MapaLiter.put('Z', new String[]{
+                "#######",
+                "     # ",
+                "    #  ",
+                "   #   ",
+                "  #    ",
+                " #     ",
+                "#######"
+        });
+        MapaLiter.put(' ', new String[]{
+                "  ",
+                "  ",
+                "  ",
+                "  ",
+                "  ",
+                "  ",
+                "  "
+        });
+    }
+    public String[] toBanner(String wejscie){
+        if(wejscie == null || wejscie.isEmpty()){
+            return new String[0];
+        }
+        wejscie = wejscie.toUpperCase();
+        int rozmiarBannera = 7;
+        String[] banner = new String[rozmiarBannera];
+        for(int i = 0; i < rozmiarBannera; i++){
+            banner[i] = "";
+        }
+        for(int i = 0; i < rozmiarBannera; i++){
+            for(int j = 0; j < wejscie.length(); j++){
+                char litera = wejscie.charAt(j);
+                if(MapaLiter.containsKey(litera)){
+                    banner[i] += MapaLiter.get(litera)[i];
+                }
+                if (j < wejscie.length() - 1) {
+                    banner[i] += " ";
+                }
+            }
+            
+        }
+        return banner;
+    }
+    
 }
--- src/main/java/uj/wmii/pwj/introduction/Reverser.java
+++ src/main/java/uj/wmii/pwj/introduction/Reverser.java
@@ -2,12 +2,28 @@ package uj.wmii.pwj.introduction;
 
 public class Reverser {
 
-    public String reverse(String input) {
-        return null;
+    public String reverse(String tekst) {
+        if (tekst == null) {
+            return null;
+        }
+        tekst = tekst.trim();
+        String pom = new StringBuilder(tekst).reverse().toString();
+        return pom;
     }
 
-    public String reverseWords(String input) {
-        return null;
+    public String reverseWords(String tekst) {
+        if (tekst == null) {
+            return null;
+        }
+        tekst = tekst.trim();
+        String[] slowa = tekst.split(" ");
+        StringBuilder odwroconyTekst = new StringBuilder();
+        for (int i = slowa.length - 1; i >= 0; i--) {
+            odwroconyTekst.append(slowa[i]);
+            if (i > 0) {
+                odwroconyTekst.append(" ");
+            }
+        }
+        return odwroconyTekst.toString();
     }
-
 }
